a44fa75b618cef9209da82ff340d1bba3bd79d0b
vandesdelca32 <vandesdelca32@7f224bc8-fadd-441f-a332-888c539a667d>
2016-04-04 10:49:54 +0000

    Tagging latest stable build
    Localization Update

---
+++ tags/release_v2.1.1/src/locale/esES.lua
@@ -0,0 +1,6 @@
+local appName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(appName, "esES", true, true)
+
+if L then
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="concat")@
+end
---
+++ tags/release_v2.1.1/src/locale/frFR.lua
@@ -0,0 +1,6 @@
+local appName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(appName, "frFR", true, true)
+
+if L then
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="concat")@
+end
