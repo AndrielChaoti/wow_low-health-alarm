ac090849d1a71d048422a263abaa8160b393a031
vandesdelca32 <vandesdelca32@7f224bc8-fadd-441f-a332-888c539a667d>
2016-10-25 23:30:04 +0000

    Tagging latest stable build

---
+++ tags/release_v2.1.5/src/locale/koKR.lua
@@ -0,0 +1,6 @@
+local appName = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(appName, "koKR", true, true)
+
+if L then
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="concat")@
+end
